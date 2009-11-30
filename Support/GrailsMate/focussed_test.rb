791af801242682d899bad3708de3d0cad1e942b8
alkemist <ld@ldaley.com>
2009-12-01 09:33:48 +1000

    Support “mapping” different test source folders to phases, in order to support running focussed web tests.

--- Support/GrailsMate/focussed_test.rb
+++ Support/GrailsMate/focussed_test.rb
@@ -1,6 +1,6 @@
 require "#{ENV['TM_SUPPORT_PATH']}/lib/exit_codes"
 
-potential_phases = ["unit", "integration", "functional", "other"]
+potential_phases = {"unit" => "unit", "integration" => "integration", "functional" => "functional", "webtest" => "functional", "other" => "other"}
 
 testclasses = []
 phases = []
@@ -13,8 +13,8 @@ ARGV.each do |t|
   if extension != "groovy" and extension != "java"
     TextMate.exit_show_tool_tip "#{test} is not a test case"
   end
-  potential_phases.each do |potential_phase|
-    phases << "--#{potential_phase}" if t =~ /test\/#{potential_phase}\//
+  potential_phases.each do |dir, potential_phase|
+    phases << "--#{potential_phase}" if t =~ /test\/#{dir}\//
   end
   testclasses << clazz.sub(/Tests$/, "")
 end
